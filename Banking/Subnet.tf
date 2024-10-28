4947705739e3aff001e0eba031651b5237fc71d1
Faris <147367370+Farisvtp@users.noreply.github.com>
2024-10-28 14:01:59 +0530

    Update Subnet.tf

--- Banking/Subnet.tf
+++ Banking/Subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "Banking_Project_Subnet" {
   vpc_id            = aws_vpc.Banking_Project_VPC.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "Banking-Project-Subnet"
   }
