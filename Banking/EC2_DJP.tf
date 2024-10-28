a559c681fad78cd310eaae8247ec554485fdff5b
Faris <147367370+Farisvtp@users.noreply.github.com>
2024-10-28 13:51:11 +0530

    Update EC2_DJP.tf

--- Banking/EC2_DJP.tf
+++ Banking/EC2_DJP.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "Banking_Project_DJP_Instance" {
   count         = 2
-  ami           = "ami-0522ab6e1ddcc7055"  # Replace with the latest Ubuntu 24.04 AMI ID
+  ami           = "ami-0dee22c13ea7a9a67"  # Replace with the latest Ubuntu 24.04 AMI ID
   instance_type = "t3.medium"
   subnet_id     = aws_subnet.Banking_Project_Subnet.id
 
